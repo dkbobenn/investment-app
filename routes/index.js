863aa2233389d2efd1d4371c70509c99c6f78150
Bo Bennetsen <Bo.Bennetsen@LEGO.com>
2022-02-07 17:24:49 +0100

    symbol name included in output on depo page

--- routes/index.js
+++ routes/index.js
@@ -14,26 +14,24 @@ const key = 'GXNI0400YMK2FWZM';
 const functionName = 'TIME_SERIES_DAILY';
 
   router.get("/stock-search", (req, res, next) => {
-   // console.log(req.query.stocks)
 
 const symbolName = req.query.stocks;
+console.log(`userinput ${symbolName}`)
 
 const apiUrl = `https://www.alphavantage.co/query?function=${functionName}&symbol=${symbolName}&apikey=${key}`;
-//console.log(`this is api url ${apiUrl}`)
-//console.log(`userinput ${symbolName}`)
     axios
     .get(apiUrl)
     .then(response => {
 
       const axiosOutputData = response.data
 
-      //let obj = JSON.parse(JSON.parse(axiosOutputData))
-      //console.log(obj)
-
-      const stockData = axiosOutputData ['Time Series (Daily)']['2022-02-04']
+      // let obj = JSON.parse(axiosOutputData)
+      // console.log(obj)
+    
+    const stockData = axiosOutputData ['Time Series (Daily)']['2022-02-04']
       //console.log('Response from API is: ', stockData);
 
-      res.render("pages/depo.hbs", { stockData })
+      res.render("pages/depo.hbs", { stockData, symbolName })
 
     })
     .catch(err => {
